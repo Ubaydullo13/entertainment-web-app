96b348534fb37d2d0fcc73d0362305eea62b1753
Ubaydullo13 <solizhonov98@gmail.com>
2024-03-05 12:12:06 +0500

    corrected

--- src/Routes/Bookmark/Bookmark.jsx
+++ src/Routes/Bookmark/Bookmark.jsx
@@ -1,20 +1,24 @@
-// import { useContext } from "react";
-// import { BookmarksContext } from "../../context/BookmarksContext";
-// import Cards from "../../components/Cards/Cards";
+import { useEffect, useState } from "react";
+
+import Cards from "../../components/Cards/Cards";
 import "./Bookmark.scss"
 
 function Bookmark() {
-  // const {bookmarks, handleBookmark} = useContext(BookmarksContext);
+  const [bookmarks, setBookmarks] = useState([]);
+  useEffect(() => {
+    const storedBookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+    setBookmarks(storedBookmarks);
+  }, []);
   return (
     <div className="bookmark">
       <h1 className="bookmark__movies-heading">Bookmarked Movies</h1>
       <div className="bookmark__movies">
-        
+        {
+              bookmarks.map((item) => (
+                <Cards key={item.id} item={item} />
+              ))
+            }
       </div>
-      <h1 className="bookmark__series-heading">Bookmarked Tv Series</h1>
-      <div className="bookmark__series">
-        
-          </div>
     </div>
   )
 }
